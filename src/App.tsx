63a4b1d7c2b290e68f521fad4172786e1bf63062
Alex Waibel <alexwaibel@users.noreply.github.com>
2020-03-25 12:10:43 -0400

    Move navbar CSS to top level

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
+import './App.css';
 
 const Home = lazy(() => import('./home/Home'));
 const Stimuli = lazy(() => import('./stimuli/Stimuli'));
